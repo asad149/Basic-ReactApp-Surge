d7aaacf4d063108c1b5e9fdaf1aaf46493b847f9
asad149 <asad.arshad.94849@gmail.com>
2020-06-21 17:54:02 +0500

    import app.css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import logo from './logo.svg';
-// import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World to all of you</h1>
+      <h1>Hello World From Asad to all of you</h1>
     </div>
   );
 }
